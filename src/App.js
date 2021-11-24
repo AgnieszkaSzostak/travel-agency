95e35bb29dce4ebcea779b0f56a8c794848430b8
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-24 21:30:11 +0100

    Add routing

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import MainLayout from './components/layout/MainLayout/MainLayout';
+
+import Home from './components/views/Home/Home';
+import Trips from './components/views/Trips/TripsContainer';
+import Trip from './components/views/Trip/TripContainer';
+import Countries from './components/views/Countries/CountriesContainer.js';
+import Country from './components/views/Country/CountryContainer.js';
+import Regions from './components/views/Regions/RegionsContainer.js'
+
+// TODO - import other views
+import Info from './components/views/Info/Info';
+import NotFound from './components/views/NotFound/NotFound';
+
+import parseTrips from './utils/parseTrips';
+import {setMultipleStates} from './redux/globalRedux';
+
+class App extends React.Component {
+  static propTypes = {
+    trips: PropTypes.array,
+    setStates: PropTypes.func,
+  }
+
+  constructor(props){
+    super(props);
+    // parse trips when App is first created
+    parseTrips(this.props.trips, this.props.setStates);
+  }
+
+  componentDidUpdate(prevProps){
+    if(prevProps.trips !== this.props.trips){
+      // parse trips again if they changed
+      parseTrips(this.props.trips, this.props.setStates);
+    }
+  }
+
+  render(){
+    return (
+      <BrowserRouter>
+        <MainLayout>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/trips' component={Trips} />
+            <Route exact path='/trip/:id' component={Trip} />
+            <Route exact path='/countries' component={Countries} />
+            <Route exact path='/country/:id' component={Country} />
+            <Route exact path='/regions' component={Regions} />
+            {/* TODO - add more routes for other views */}
+            <Route exact path='/info' component={Info} />
+            <Route path='*' component={NotFound} />
+          </Switch>
+        </MainLayout>
+      </BrowserRouter>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  trips: state.trips,
+});
+
+const mapDispatchToProps = dispatch => ({
+  setStates: newState => dispatch(setMultipleStates(newState)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
