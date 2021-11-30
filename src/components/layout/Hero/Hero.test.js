abfbb05657f31bc655118140e7de7a6c0d4a8cfc
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-30 20:10:30 +0100

    Add Hero.test.js

---
+++ src/components/layout/Hero/Hero.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Hero from './Hero';
+
+describe('Component Hero', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<Hero titleText='Lorem ipsum' />);
+    expect(component).toBeTruthy();
+  });
+  it('should throw error without required props', () => {
+    expect(() => shallow(<Hero />)).toThrow();
+  });
+  it('should render correct title', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const component = shallow(<Hero titleText={expectedTitle} />);
+  
+    const renderedTitle = component.find('.title').text();
+    expect(renderedTitle).toEqual(expectedTitle);
+  });
+});
